3fcff44d6da71fc9b8dd3595f02efa6a6723fafa
David Saxon <davidesaxon@gmail.com>
2017-04-02 23:47:20 +1200

    update for windows 10

--- tools/w_build.bat
+++ tools/w_build.bat
@@ -1,5 +1,3 @@
-set VCTargetsPath=c:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120
-
 msbuild ..\ArcaneLog.sln /v:n /p:Configuration=arcanelog
 msbuild ..\ArcaneLog.sln /v:n /p:Configuration=arcanelog_shared
 msbuild ..\ArcaneLog.sln /v:n /p:Configuration=tests
